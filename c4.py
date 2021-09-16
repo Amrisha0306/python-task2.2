7b2fa90a832713cfd79bdbac916cde91ec25e325
Amrisha0306 <70196110+Amrisha0306@users.noreply.github.com>
2021-09-16 21:53:23 +0530

    Add files via upload

---
+++ c4.py
@@ -0,0 +1,10 @@
+list1=[12,-7,5,64,-14]
+list2=[12,14,-95,3]
+for item in list1: 
+        if item < 0: 
+            list1.remove(item)
+print(list1)
+for item in list2: 
+        if item < 0: 
+            list2.remove(item)
+print(list2)
